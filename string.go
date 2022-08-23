53cfceff1ad8ac57659be8212e0bf8d659408f13
Allen-wang-cn <whl2009_king@163.com>
2022-08-23 14:14:27 +0800

    提交代码

--- string.go
+++ string.go
@@ -2,12 +2,22 @@
  * @Description: strings operate tools
  * @Author: Allen
  * @since: 2022-08-23 11:54:58
- * @lastTime: 2022-08-23 11:57:28
+ * @lastTime: 2022-08-23 14:09:36
  * @LastAuthor: Allen
  * @FilePath: \uitls\string.go
  */
 package utils
 
+/**
+ * @Description: 获取字符串长度 如果是空串 返回 0  如果非空 转为rune切片 再获取长度
+ * @since: 2022-08-23 14:08:29
+ * @param {string} param
+ * @return {*}
+ */
 func StrLen(param string) int64 {
-	return int64(len(param))
+	if param == "" {
+		return 0
+	}
+	ru := []rune(param)
+	return int64(len(ru))
 }
