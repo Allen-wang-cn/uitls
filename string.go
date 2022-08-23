b42240ecfd2a84a6b7dee37895c5e7f21044106a
Allen-wang-cn <whl2009_king@163.com>
2022-08-23 11:57:57 +0800

    字符串处理工具

---
+++ string.go
@@ -0,0 +1,13 @@
+/**
+ * @Description: strings operate tools
+ * @Author: Allen
+ * @since: 2022-08-23 11:54:58
+ * @lastTime: 2022-08-23 11:57:28
+ * @LastAuthor: Allen
+ * @FilePath: \uitls\string.go
+ */
+package utils
+
+func StrLen(param string) int64 {
+	return int64(len(param))
+}
